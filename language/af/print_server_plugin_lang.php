051d616ef042b5c7c9d9c65acef15f2a1ade618d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:36:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/af/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Druk Beleid Server bied toegangsbeheer tot die afdruk bediener app.';
+$lang['print_server_plugin_app_name'] = 'Print Server Beleid';
+$lang['print_server_plugin_print_server_administrator'] = 'Print Server Administrator';
