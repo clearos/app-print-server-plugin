549ce40c2780714905831286cc94549c9a37d50a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:41:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/it/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Stampa Politiche server forniscono il controllo dell&#39;accesso al app server di stampa.';
+$lang['print_server_plugin_app_name'] = 'Politiche Print Server';
+$lang['print_server_plugin_print_server_administrator'] = 'Print Server Administrator';
