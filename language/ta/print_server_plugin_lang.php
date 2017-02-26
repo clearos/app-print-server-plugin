9b8e1cb52e8385b6f406143758c1d2cd6f223902
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:46:39 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'பிரிண்ட் சர்வர் கொள்கைகள் பிரிண்ட் சர்வர் பயன்பாட்டை கட்டுப்படுத்த உதவும்.';
+$lang['print_server_plugin_app_name'] = 'பிரிண்ட் சர்வர் கொள்கைகள்';
+$lang['print_server_plugin_print_server_administrator'] = 'பிரிண்ட் சர்வர் நிர்வாகி';
