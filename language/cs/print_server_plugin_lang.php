05b82c946d8fff46a499232e7672918d6d0b26c6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:38:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Politiky tiskový server zajišťuje řízení přístupu k aplikaci Print Server.';
+$lang['print_server_plugin_app_name'] = 'Politiky tiskového serveru';
+$lang['print_server_plugin_print_server_administrator'] = 'Správce tiskového serveru';
