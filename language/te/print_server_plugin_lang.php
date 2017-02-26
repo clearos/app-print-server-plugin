33edf7ab9e37f26df50ce2eafa1762226174a62b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:46:47 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'ప్రింట్ సర్వర్ విధానాలు ప్రింటర్ సర్వర్ అనువర్తనానికి ప్రాప్తి నియంత్రణ అందిస్తాయి.';
+$lang['print_server_plugin_app_name'] = 'ప్రింట్ సర్వర్ విధానాలు';
+$lang['print_server_plugin_print_server_administrator'] = 'ప్రింట్ సర్వర్ నిర్వహణాధికారి';
