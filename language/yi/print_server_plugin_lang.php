4a9a6d5cba59ea83e664bbf7c37d53737696cc93
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:48:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'דרוק דעם סערווירער פּאַלאַסיז צושטעלן צוטריט קאָנטראָל צו די דרוק סערווירער אַפּ.';
+$lang['print_server_plugin_app_name'] = 'דרוק דעם סערווירער פּאָליסיעס';
+$lang['print_server_plugin_print_server_administrator'] = 'דרוק דעם סערווירער אַדמיניסטראַטאָר';
