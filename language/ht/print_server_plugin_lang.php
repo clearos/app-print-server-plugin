6ee4602383eb08556799ee9304084f13cb2feffa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:40:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Enprime Règleman sèvè bay aksè kontwòl nan app a ekri an lèt detache sèvè.';
+$lang['print_server_plugin_app_name'] = 'Règleman Enprime sèvè';
+$lang['print_server_plugin_print_server_administrator'] = 'Enprime sèvè Administratè';
