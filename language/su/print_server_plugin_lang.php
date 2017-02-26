bec54a4457c0ecee3b79e0b3a373904bd6ac04f4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:46:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Print Kawijakan Server nyadiakeun kadali aksés ka app print Server.';
+$lang['print_server_plugin_app_name'] = 'Kawijakan print Server';
+$lang['print_server_plugin_print_server_administrator'] = 'Print Server Administrator';
