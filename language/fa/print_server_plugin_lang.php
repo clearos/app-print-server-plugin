48b97fb989e28a1167eeab85a293d662dbb463e6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:44:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'سیاست چاپ و سرور ارائه کنترل دسترسی به برنامه چاپ و سرور.';
+$lang['print_server_plugin_app_name'] = 'سیاست چاپ و سرور';
+$lang['print_server_plugin_print_server_administrator'] = 'چاپ مدیر سرور';
