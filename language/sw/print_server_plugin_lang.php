3e33baa3170756adff1515ce75c903ba00cb4914
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:46:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Magazeti Sera Server kutoa upatikanaji wa kudhibiti na programu magazeti Server.';
+$lang['print_server_plugin_app_name'] = 'Sera Print Server';
+$lang['print_server_plugin_print_server_administrator'] = 'Magazeti Msimamizi Server';
