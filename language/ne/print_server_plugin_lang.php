b3f1b64f4565b6efbbe47c90295bd0b6e9a9313b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:43:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'सर्भरमुद्रणगर्नुहोस् नीतिहरू मुद्रण सर्भर अनुप्रयोग पहुँच नियन्त्रण प्रदान।';
+$lang['print_server_plugin_app_name'] = 'सर्भरमुद्रणगर्नुहोस् नीतिहरू';
+$lang['print_server_plugin_print_server_administrator'] = 'सर्भरमुद्रणगर्नुहोस् प्रशासक';
