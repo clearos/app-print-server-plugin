a41d03905900aa46817161df03ac66f4a000f6dd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:39:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gl/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Políticas servidor de impresión fornecen control de acceso para a aplicación Imprimir Server.';
+$lang['print_server_plugin_app_name'] = 'Políticas do servidor de impresión';
+$lang['print_server_plugin_print_server_administrator'] = 'Administrador de servidor de impresión';
