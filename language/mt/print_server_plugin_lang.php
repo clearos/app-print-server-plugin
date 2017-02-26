4f318d7a796b9a87fe19c369a0a12c5711515466
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:43:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Politiki Server Stampa jipprovdu kontroll ta &#39;aċċess għas-app Stampa Server.';
+$lang['print_server_plugin_app_name'] = 'Politiki Stampa Server';
+$lang['print_server_plugin_print_server_administrator'] = 'Stampa Server Amministratur';
