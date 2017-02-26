a9f3854d8a8edd7cffd4ca1758edc63aee322b27
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:47:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Máy chủ Chính sách In cung cấp kiểm soát truy cập vào các ứng dụng Print Server.';
+$lang['print_server_plugin_app_name'] = 'Chính sách Print Server';
+$lang['print_server_plugin_print_server_administrator'] = 'In Quản trị máy chủ';
