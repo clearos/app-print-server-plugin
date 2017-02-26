af1cda443eddad18aa026709960da83afd98b598
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:37:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Print Server politike osigurati kontrolu pristupa Print Server aplikacije.';
+$lang['print_server_plugin_app_name'] = 'Print Server politike';
+$lang['print_server_plugin_print_server_administrator'] = 'Print Server Administrator';
