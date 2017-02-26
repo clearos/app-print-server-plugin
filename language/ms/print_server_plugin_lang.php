7d09ecd89b593929dfd1c859ce3aa88f332db63a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:43:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Cetakan Server menyediakan kawalan akses kepada aplikasi Cetak Server.';
+$lang['print_server_plugin_app_name'] = 'Dasar Server';
+$lang['print_server_plugin_print_server_administrator'] = 'Cetak Administrator Server';
