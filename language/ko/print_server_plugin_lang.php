9e9abf76f91d374b1c062eaf765f974a7a8e40f9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:42:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = '인쇄 서버 정책은 프린트 서버 응용 프로그램에 대한 액세스 제어를 제공합니다.';
+$lang['print_server_plugin_app_name'] = '인쇄 서버 정책';
+$lang['print_server_plugin_print_server_administrator'] = '프린트 서버 관리';
