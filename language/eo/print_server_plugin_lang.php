934691cadec6000861483062b0f2e5a4c8ce922d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:39:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Print Servilo Politikoj havigi alirkontrolo al la servanto de impreso app.';
+$lang['print_server_plugin_app_name'] = 'Print Servilo Politikoj';
+$lang['print_server_plugin_print_server_administrator'] = 'Print Server Administrator';
