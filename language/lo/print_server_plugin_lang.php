65f1223eff6ef0cadccae6ab5671519641a5dd92
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:42:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lo/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'ນະໂຍບາຍ Server ພິມໃຫ້ການຄວບຄຸມການເຂົ້າເຖິງ app ພິມ Server.';
+$lang['print_server_plugin_app_name'] = 'ນະໂຍບາຍ Print Server';
+$lang['print_server_plugin_print_server_administrator'] = 'Administrator Server ພິມ';
