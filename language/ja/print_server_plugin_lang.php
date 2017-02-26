18aea5b2cbc14efc692d5a959e92789cde6f3e95
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:41:29 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'プリントサーバーポリシーは、プリントサーバアプリへのアクセス制御を提供します。';
+$lang['print_server_plugin_app_name'] = 'プリントサーバーポリシー';
+$lang['print_server_plugin_print_server_administrator'] = 'プリントサーバー管理者';
