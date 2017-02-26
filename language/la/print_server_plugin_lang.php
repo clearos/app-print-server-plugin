38522b7347ee579b6107d1863ee57d12d463914c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:42:19 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Servo Policies providere accessum ad imperium Print Print Server App.';
+$lang['print_server_plugin_app_name'] = 'Print Server Politiae';
+$lang['print_server_plugin_print_server_administrator'] = 'Print Administrator Server';
