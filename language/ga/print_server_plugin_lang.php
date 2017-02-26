0284b14ec28f6e8ea7b143cc775bec731f780212
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:40:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'a chur ar fáil Polasaithe Freastalaí Priontáil rialú rochtain ar an app Freastalaí Priontáil.';
+$lang['print_server_plugin_app_name'] = 'Polasaithe Freastalaí Priontáil';
+$lang['print_server_plugin_print_server_administrator'] = 'Riarthóir Freastalaí Priontáil';
