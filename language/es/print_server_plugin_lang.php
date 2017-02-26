4c0be932634c57e7f9a81e063ac8844ac5e9955b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:46:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Condiciones del servidor de impresión proporcionan un control de acceso a la aplicación de servidor de impresión.';
+$lang['print_server_plugin_app_name'] = 'Las políticas del servidor de impresión';
+$lang['print_server_plugin_print_server_administrator'] = 'Servidor de impresión administrador';
