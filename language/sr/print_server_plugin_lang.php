b25447105ed4413b3fc7db95b3f282f2a42f3964
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:45:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Принт Политике Сервер обезбеди контролу приступа на принт сервер апликације.';
+$lang['print_server_plugin_app_name'] = 'Принт сервер Политике';
+$lang['print_server_plugin_print_server_administrator'] = 'Принт сервер администратор';
