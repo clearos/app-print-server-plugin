abd66d2cf2b080e293d280bc36f10d06b27339c0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:37:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'প্রিন্ট সার্ভার নীতি প্রিন্ট সার্ভার অ্যাপ্লিকেশন অ্যাক্সেস নিয়ন্ত্রণ প্রদান.';
+$lang['print_server_plugin_app_name'] = 'প্রিন্ট সার্ভার নীতিসমূহ';
+$lang['print_server_plugin_print_server_administrator'] = 'প্রিন্ট সার্ভার প্রশাসক';
