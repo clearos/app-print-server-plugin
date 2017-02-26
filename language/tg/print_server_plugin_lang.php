6d5469737221ad3cb3d6e9a01c9e80e5cac1b0eb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:46:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Сиёсати Print Server таъмин назорати дастрасӣ ба барнома чопӣ Server.';
+$lang['print_server_plugin_app_name'] = 'Сиёсати Чоп Server';
+$lang['print_server_plugin_print_server_administrator'] = 'Чоп администратори сервер';
