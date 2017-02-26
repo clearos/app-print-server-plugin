42bfaba359b8c13b3e6f0f4bf5d36f2b03693619
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:39:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Print Server käytännöt tarjoavat kulunvalvonta tulostuspalvelimeen sovelluksen.';
+$lang['print_server_plugin_app_name'] = 'Print Server säännöt';
+$lang['print_server_plugin_print_server_administrator'] = 'Print Server Administrator';
