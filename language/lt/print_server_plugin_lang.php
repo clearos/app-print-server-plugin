000ceb83f6aaf3d93edf2e9f6feecb2a332456a2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:42:35 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Spausdinimo serveriai politika teikti prieigos kontrolės prie Spausdinimo serveriai app.';
+$lang['print_server_plugin_app_name'] = 'Spausdinimo serveriai politika';
+$lang['print_server_plugin_print_server_administrator'] = 'Spausdinti serverio administratorius';
