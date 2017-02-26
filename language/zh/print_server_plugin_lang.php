f33080c09b52cb009c872ea9942044a5175490c2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:38:38 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = '打印服务器策略提供访问控制到打印服务器应用程序。';
+$lang['print_server_plugin_app_name'] = '打印服务器策略';
+$lang['print_server_plugin_print_server_administrator'] = '打印服务器管理员';
