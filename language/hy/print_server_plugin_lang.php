c318a8dcf1152f4252f343c53906fb902001260b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:37:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Print Server քաղաքականությունը ապահովում է մուտքի վերահսկման, ինչպես նաեւ տպագիր Server հավելվածի:';
+$lang['print_server_plugin_app_name'] = 'Print Server քաղաքականություն';
+$lang['print_server_plugin_print_server_administrator'] = 'Print Server Administrator';
