cc3670c8c0310e48764863b0f1f981dbede5c8dc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:48:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Polisïau Gweinydd Print darparu rheolaeth mynediad i&#39;r app gweinydd argraffu.';
+$lang['print_server_plugin_app_name'] = 'Polisïau Print Gweinydd';
+$lang['print_server_plugin_print_server_administrator'] = 'Print Gweinyddwr Gweinydd';
