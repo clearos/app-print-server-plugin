7a668161db9f599a3b15acae829b6fa72d9b6565
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:45:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Print Siyaasadaha Server bixiyaan helitaanka app Server Print ee gacanta.';
+$lang['print_server_plugin_app_name'] = 'Siyaasadaha Print Server';
+$lang['print_server_plugin_print_server_administrator'] = 'Print Maamulaha Server';
