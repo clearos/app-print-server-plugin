b07a250a15563832dddd265619bac6313188b0f0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:38:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'ပရင့်ထုတ်ရန် Server ကိုမူဝါဒများအ Print Server ကို app ကိုအသုံးပြုခွင့်ကိုထိန်းချုပ်ပေး။';
+$lang['print_server_plugin_app_name'] = 'ပရင့်ထုတ်ရန် Server ကိုမူဝါဒများ';
+$lang['print_server_plugin_print_server_administrator'] = 'ပရင့်ထုတ်ရန်ဆာဗာအုပ်ချုပ်ရေးမှူး';
