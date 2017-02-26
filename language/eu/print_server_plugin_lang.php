51bf757d70bbfd3b24e6e70548e28cc7bd0252d1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:37:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Print Server Politikak eskaintzen sarbide kontrol inprimatzeko zerbitzaria aplikazioa da.';
+$lang['print_server_plugin_app_name'] = 'Print Server Politikak';
+$lang['print_server_plugin_print_server_administrator'] = 'Print Server Administrator';
