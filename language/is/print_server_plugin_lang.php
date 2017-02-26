24ad20f1f611f34f9b35d1bf7b16da863d6a7e8e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:41:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Prentþjón Reglur veita aðgangsstýringu að prentþjóninum app.';
+$lang['print_server_plugin_app_name'] = 'Print Server Reglur';
+$lang['print_server_plugin_print_server_administrator'] = 'Print Server Administrator';
