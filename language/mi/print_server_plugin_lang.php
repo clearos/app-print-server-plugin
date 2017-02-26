50e8ea4c1c860b74e2cd0084a0812964cfd4aa7b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:43:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'whakarato kaupapa here Tūmau Tā mana uru ki te taupānga Tūmau Tā.';
+$lang['print_server_plugin_app_name'] = 'Kaupapa here Tūmau Tā';
+$lang['print_server_plugin_print_server_administrator'] = 'Kaiwhakahaere Tūmau Tā';
