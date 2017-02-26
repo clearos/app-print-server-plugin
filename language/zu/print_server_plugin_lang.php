7d37bca739a1c65d910f1fbad28e8050daa147f4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:48:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Phrinta Izinqubomgomo Server ukunikeza control ukufinyelela lokusebenza ukuphrinta Server.';
+$lang['print_server_plugin_app_name'] = 'Izinqubomgomo Phrinta Server';
+$lang['print_server_plugin_print_server_administrator'] = 'Phrinta Server Administrator';
