d3ac0c51bd871ea66bcfe3314b47d526e94bfb09
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:39:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Εκτύπωση των Server παρέχει έλεγχο πρόσβασης στην εφαρμογή Print Server.';
+$lang['print_server_plugin_app_name'] = 'Πολιτικές Print Server';
+$lang['print_server_plugin_print_server_administrator'] = 'Εκτύπωση διαχειριστή διακομιστή';
