fb34345d2c5bdc80b275fd575fd20f5710137dfc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:43:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Хэвлэх Сервер Бодлого хэвлэх сервер програм руу хандалтын хяналтыг олгодог.';
+$lang['print_server_plugin_app_name'] = 'Хэвлэх Серверүүд бодлого';
+$lang['print_server_plugin_print_server_administrator'] = 'Хэвлэх Серверүүд Administrator';
