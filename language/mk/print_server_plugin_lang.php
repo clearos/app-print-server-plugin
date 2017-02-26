4e741d7e89fd42c545ff27b87d807781bed033f0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:43:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Политики сервер за печатење обезбеди контрола на пристап на апликацијата сервер за печатење.';
+$lang['print_server_plugin_app_name'] = 'Политики сервер за печатење';
+$lang['print_server_plugin_print_server_administrator'] = 'Верзија за администраторот на серверот';
