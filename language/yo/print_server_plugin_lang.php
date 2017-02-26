7327c1a52ebb75c772fa8d78ddf5a26c74460963
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:48:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Print Server imulo pese wiwọle iṣakoso si awọn tìte Server app.';
+$lang['print_server_plugin_app_name'] = 'Print Server imulo';
+$lang['print_server_plugin_print_server_administrator'] = 'Print Server IT';
