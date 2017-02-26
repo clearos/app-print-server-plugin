4431053e5a3163f86578a8e99523e725b4a7c76a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:42:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Print Server Policies fanaraha-maso dia manome fahafahana ny atao pirinty Server App.';
+$lang['print_server_plugin_app_name'] = 'Print Server Policies';
+$lang['print_server_plugin_print_server_administrator'] = 'Print Server Administrator';
