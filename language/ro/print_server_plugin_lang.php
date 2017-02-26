0e45a9654639dc9a5ec5e830103b2d5f22b79094
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:44:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Politicile Server de imprimare oferă un control de acces la aplicația Server PRINTAȚI.';
+$lang['print_server_plugin_app_name'] = 'Politicile Server de imprimare';
+$lang['print_server_plugin_print_server_administrator'] = 'Administrator Server de imprimare';
