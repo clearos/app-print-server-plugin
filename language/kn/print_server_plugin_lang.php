53358a1e40f01b10d76adce076ecb99364ec5f97
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:42:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'ಮುದ್ರಣ ಸರ್ವರ್ ನೀತಿಗಳು ಪ್ರಿಂಟ್ ಸರ್ವರ್ ಅಪ್ಲಿಕೇಶನ್ ಪ್ರವೇಶವನ್ನು ನಿಯಂತ್ರಣ ಒದಗಿಸುತ್ತದೆ.';
+$lang['print_server_plugin_app_name'] = 'ಮುದ್ರಣ ಸರ್ವರ್ ನೀತಿಗಳು';
+$lang['print_server_plugin_print_server_administrator'] = 'ಮುದ್ರಣ ಸರ್ವರ್ ನಿರ್ವಾಹಕ';
