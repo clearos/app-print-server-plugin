39952c13c3c02a73e301d1c67bdf24df9947e161
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:41:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Басып шығару сервер саясаты баспа серверіне бағдарламаға қатынасу бақылауын қамтамасыз етеді.';
+$lang['print_server_plugin_app_name'] = 'Басып шығару серверінің саясаты';
+$lang['print_server_plugin_print_server_administrator'] = 'Басып шығару сервер әкімшісі';
