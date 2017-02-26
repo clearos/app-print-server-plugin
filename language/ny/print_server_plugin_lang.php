218aa222ffa22d5ed20d859f482db8d81f1fd087
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:38:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Sindikizani Ndondomeko Server imapatsa kulamulira kwa app kusindikiza Server.';
+$lang['print_server_plugin_app_name'] = 'Ndondomeko kusindikiza Server';
+$lang['print_server_plugin_print_server_administrator'] = 'Sindikizani Woyang&#39;anira Server';
