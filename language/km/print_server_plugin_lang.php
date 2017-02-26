34447a3006d2ddb96db4b52d65eb4b950c92da59
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:41:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'ម៉ាស៊ីនបម្រើបោះពុម្ពផ្តល់ជូននូវគោលនយោបាយត្រួតត្រាការចូលដំណើរការទៅកាន់កម្មវិធីម៉ាស៊ីនបម្រើបោះពុម្ព។';
+$lang['print_server_plugin_app_name'] = 'ម៉ាស៊ីនបម្រើបោះពុម្ពគោលនយោបាយ';
+$lang['print_server_plugin_print_server_administrator'] = 'អ្នកគ្រប់គ្រងម៉ាស៊ីនបម្រើបោះពុម្ព';
