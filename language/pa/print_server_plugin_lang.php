61d6f5155fb7b885067e43d442140132628942f6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:44:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'ਪ੍ਰਿੰਟ ਸਰਵਰ ਡਰਾਇਰ ਪ੍ਰਿੰਟ ਸਰਵਰ ਐਪਲੀਕੇਸ਼ ਨੂੰ ਪਹੁੰਚ ਕੰਟਰੋਲ ਮੁਹੱਈਆ ਕਰਦਾ ਹੈ.';
+$lang['print_server_plugin_app_name'] = 'ਪ੍ਰਿੰਟ ਸਰਵਰ ਡਰਾਇਰ';
+$lang['print_server_plugin_print_server_administrator'] = 'ਪ੍ਰਿੰਟ ਸਰਵਰ ਪਰਬੰਧਕ';
