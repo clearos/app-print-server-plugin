9e4f5a682220838a75900c7e38f18cb1cfc7f518
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:40:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'પ્રિન્ટ સર્વર નીતિઓ પ્રિન્ટ સર્વર એપ્લિકેશન ઍક્સેસ નિયંત્રણ પૂરો પાડે છે.';
+$lang['print_server_plugin_app_name'] = 'પ્રિન્ટ સર્વર નીતિઓ';
+$lang['print_server_plugin_print_server_administrator'] = 'પ્રિન્ટ સર્વર સંચાલક';
