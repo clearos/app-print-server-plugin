94a13bc3b56fb2722eeb1a293f859186f22a178f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:40:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Print Server Asiri samar da damar iko da Fitar Server app.';
+$lang['print_server_plugin_app_name'] = 'Print Server Asiri';
+$lang['print_server_plugin_print_server_administrator'] = 'Print Server Administrator';
