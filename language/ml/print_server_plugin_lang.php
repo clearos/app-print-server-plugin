c6ea14c79b77e8514487afc16ec2623b73b3ab2d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:43:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'പ്രിന്റ് സെർവർ നയങ്ങള് പ്രിന്റ് സെർവർ അപ്ലിക്കേഷൻ ആക്സസ് നിയന്ത്രണം നൽകുന്നു.';
+$lang['print_server_plugin_app_name'] = 'പ്രിന്റ് സെർവർ നയങ്ങള്';
+$lang['print_server_plugin_print_server_administrator'] = 'പ്രിന്റ് സെർവർ അഡ്മിനിസ്ട്രേറ്റർ';
