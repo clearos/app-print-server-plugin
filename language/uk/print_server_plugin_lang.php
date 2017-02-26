8d317d35ae0d50af58592df06ca26ac0b3f8af3f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:47:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uk/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Друк політик сервера забезпечують контроль доступу до додатка сервера друку.';
+$lang['print_server_plugin_app_name'] = 'Політики сервера друку';
+$lang['print_server_plugin_print_server_administrator'] = 'Друк Адміністратор сервера';
