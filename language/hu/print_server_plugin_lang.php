7a7d070d2b7cee82cab642f11c358e9e89e1babe
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:40:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Print Server házirendje hozzáférést biztosít a vezérlést a nyomtatószerver alkalmazást.';
+$lang['print_server_plugin_app_name'] = 'Print Server házirendje';
+$lang['print_server_plugin_print_server_administrator'] = 'Print Server Administrator';
