8e096ed39e168325c4d351817e3095329a9ecfd0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:46:31 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Skrivarserver Policies ger åtkomstkontroll till skrivarservern appen.';
+$lang['print_server_plugin_app_name'] = 'Skrivarserverinställningar Policies';
+$lang['print_server_plugin_print_server_administrator'] = 'Print Server administratörs';
