7338488e61737a91fcb8454f41025adad70fb952
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:47:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Chop etish uchun Server siyosati Chop etish Server ilova kirish nazoratini ta&#39;minlash.';
+$lang['print_server_plugin_app_name'] = 'Chop etish uchun Server siyosati';
+$lang['print_server_plugin_print_server_administrator'] = 'Chop etish uchun Server Administrator';
