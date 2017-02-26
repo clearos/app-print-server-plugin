d8c2f814a40f47f5c644000d99a3db4fa9f00386
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:47:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'นโยบาย Print Server ให้การควบคุมการเข้าถึงแอพพลิเค Print Server';
+$lang['print_server_plugin_app_name'] = 'นโยบาย Print Server';
+$lang['print_server_plugin_print_server_administrator'] = 'ผู้ดูแลระบบเซิร์ฟเวอร์การพิมพ์';
