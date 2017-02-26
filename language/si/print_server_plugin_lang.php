5ecc74decbc6fa2a971b9b71f9ef17b083c8c68a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:45:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'මුද්රණය සේවාදායකය ප්රතිපත්ති මුද්රණය සේවාදායකය යෙදුමට ප්රවේශ පාලක ලබා දෙයි.';
+$lang['print_server_plugin_app_name'] = 'මුද්රණය සේවාදායකය ප්රතිපත්ති';
+$lang['print_server_plugin_print_server_administrator'] = 'මුද්රණය සේවාදායකය පරිපාලක';
