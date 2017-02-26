b573a4368ba2cf0dea180b0321e5b0f18c73dabb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:41:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Bipute Server Atumatu-enye ohere akara na-ebipụta Server ngwa.';
+$lang['print_server_plugin_app_name'] = 'Bipute Server Atumatu';
+$lang['print_server_plugin_print_server_administrator'] = 'Bipute Server Administrator';
