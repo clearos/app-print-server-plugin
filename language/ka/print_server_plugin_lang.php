20c93aee196120c541bb3921f05a395c6ad213d9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:41:37 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Print Server პოლიტიკის უზრუნველყოს კონტროლის ბეჭდვის სერვერზე app.';
+$lang['print_server_plugin_app_name'] = 'Print Server პოლიტიკა';
+$lang['print_server_plugin_print_server_administrator'] = 'Print Server Administrator';
