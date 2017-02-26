dced9cd3e1efcbbcd820b85bd4f72dafad3667ab
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:45:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'Print Maano Server fana phihlello ya taolo ya ho app e sa hatisoa Server.';
+$lang['print_server_plugin_app_name'] = 'Maano Print Server';
+$lang['print_server_plugin_print_server_administrator'] = 'Print Molaodi Server';
