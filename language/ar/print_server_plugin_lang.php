23e85fecb8bc0e35b9053eae9c5a3a23afb926ef
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-26 23:37:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/print_server_plugin_lang.php
@@ -0,0 +1,5 @@
+<?php
+
+$lang['print_server_plugin_app_description'] = 'توفر سياسات خادم الطباعة التحكم في الوصول إلى التطبيق ملقم الطباعة.';
+$lang['print_server_plugin_app_name'] = 'سياسات خادم الطباعة';
+$lang['print_server_plugin_print_server_administrator'] = 'مسؤول ملقم الطباعة';
